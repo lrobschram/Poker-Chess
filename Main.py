2345318932c291a642b883d895e5dce7e94d150a
lrobschram <lukeschram7@gmail.com>
2026-01-06 17:43:40 -0500

    Cleaned up Main.py for generalization

--- Main.py
+++ Main.py
@@ -6,47 +6,37 @@ from PokerScreen import PokerScreen
 from PlacementScreen import PlacementScreen
 from AttackScreen import AttackScreen
 
-
 pygame.init()
 screen = pygame.display.set_mode((560, 560))
 pygame.display.set_caption("Board Game")
 
 game = Game()
 
+screens = {
+    Screen.POKER: PokerScreen(),
+    Screen.PLACEMENT: PlacementScreen(),
+    Screen.MOVEMENT: MovementScreen(),
+    Screen.ATTACK: AttackScreen(),
+}
+
 curr_phase = Screen.MOVEMENT
-curr_screen = MovementScreen()
+curr_screen = screens[curr_phase]
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            break
 
-        if curr_phase == Screen.POKER:
-            curr_phase = PokerScreen.handle_event(event, game)
+        new_phase = curr_screen.handle_event(event, game)
 
-        elif curr_phase == Screen.PLACEMENT:
-            curr_phase = PlacementScreen.handle_event(event, game)
+        if new_phase != curr_phase:
+            # TODO add some "onExit" and "onEnter" methods
+            curr_phase = new_phase
+            curr_screen = screens[curr_phase]
 
-        elif curr_phase == Screen.MOVEMENT:
-            curr_phase = curr_screen.handle_event(event, game)
-        
-        elif curr_phase == Screen.ATTACK:
-            curr_phase = AttackScreen.handle_event(event, game)
-
-    if curr_phase == Screen.POKER:
-        PokerScreen.draw(screen, game)
-
-    elif curr_phase == Screen.PLACEMENT:
-        PlacementScreen.draw(screen, game)
-
-    elif curr_phase == Screen.MOVEMENT:
-        curr_screen.draw(screen, game)
-
-    elif curr_phase == Screen.ATTACK:
-        AttackScreen.draw(screen, game)
-
+    curr_screen.draw(screen, game)
     pygame.display.flip()
-
 
 pygame.quit()
